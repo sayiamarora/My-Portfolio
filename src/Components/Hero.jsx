c9bda377d258c94d08a0e651113f44bb5fc585dd
Sayiam <sayiam0127@gmail.com>
2024-10-17 17:39:55 +0530

    Framer Motion Applied(C6)

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,4 +1,22 @@
 import profilePic from "../assets/sayiamaroraimage.png";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+};
 
 function Hero() {
   return (
@@ -6,22 +24,41 @@ function Hero() {
       <div className="flex flex-wrap lg:flex-row-reverse ">
         <div className="flex justify-center w-full lg:w-1/2 lg:pl-10">
           <div className="flex justify-center rounded-3xl bg-gray-800 w-[30rem] hover:scale-[1.05] ease-in-out transition-all duration-500">
-            <img
+            <motion.img
               src={profilePic}
               alt="Sayiam Arora"
               className="h-[20rem] lg:h-[34rem] rounded-3xl"
+              width={650}
+              height={650}
+              initial={{ x: 0, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
             />
           </div>
         </div>
         <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start mt-10">
-            <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">
+          <motion.div
+            initial="hidden"
+            animate="visible"
+            variants={containerVariants}
+            className="flex flex-col items-center lg:items-start mt-10"
+          >
+            <motion.h2
+              variants={childVariants}
+              className="pb-2 text-4xl tracking-tighter lg:text-8xl"
+            >
               Sayiam Arora
-            </h2>
-            <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h2>
+            <motion.span
+              variants={childVariants}
+              className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent"
+            >
               Data Scientist
-            </span>
-            <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+            </motion.span>
+            <motion.p
+              variants={childVariants}
+              className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter"
+            >
               As a passionate data scientist, I am eager to delve into the
               exciting world of data-driven insights. Equipped with a strong
               foundation in data analysis and visualization, I am confident in
@@ -29,8 +66,9 @@ function Hero() {
               datasets. My enthusiasm for uncovering hidden patterns and trends
               motivates me to continuously develop my skills and contribute to
               innovative solutions.
-            </p>
-            <a
+            </motion.p>
+            <motion.a
+              variants={childVariants}
               href="Sayiam_Arora_CV1.pdf"
               target="_blank"
               rel="noopener noreferrer"
@@ -38,8 +76,8 @@ function Hero() {
               className="bg-gray-100 rounded-full px-4 py-2 text-sm font-bold  text-black mb-10 hover:bg-gray-300 ease-in-out transition-all duration-500 hover:-translate-y-1"
             >
               Download Resume
-            </a>
-          </div>
+            </motion.a>
+          </motion.div>
         </div>
       </div>
     </div>
--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -16,122 +16,238 @@ import { SiGooglecolab } from "react-icons/si";
 import { DiJava } from "react-icons/di";
 import { DiJira } from "react-icons/di";
 import { DiSqllite } from "react-icons/di";
+import { motion, transform } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 function Technologies() {
   return (
     <div className="pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="group">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transtion={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2.5)}
+        >
           <FaPython className="text-7xl " />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Python
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+        >
           <SiTableau className="text-7xl "></SiTableau>
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Tableau
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+        >
           <DiGithub className="text-7xl " />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Github
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2)}
+        >
           <SiR className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             R
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(6)}
+        >
           <DiSqllite className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             SQL
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(4)}
+        >
           <DiGit className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Git
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+        >
           <SiJupyter className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Jupyter
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(4)}
+        >
           <SiGooglecolab className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Colab
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(4)}
+        >
           <DiJava className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Java
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+        >
           <SiCplusplus className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             C++
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(4)}
+        >
           <DiAws className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             AWS
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+        >
           <DiJira className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Jira
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+        >
           <DiHtml5 className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             HTML
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(4)}
+        >
           <DiCss3 className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             CSS
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+        >
           <DiDjango className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Django
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+        >
           <RiTailwindCssFill className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Tailwind
           </div>
-        </div>
+        </motion.div>
 
-        <div className="group">
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+        >
           <DiJavascript className="text-7xl" />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Javascript
           </div>
-        </div>
-        <div className="group">
+        </motion.div>
+        <motion.div
+          className="group"
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+        >
           <RiReactjsLine className="text-7xl " />
           <div className="text-center opacity-0 group-hover:opacity-100 invisible group-hover:visible transition-all duration-700 ease-in-out transform translate-y-[-20px] group-hover:translate-y-0">
             Reactjs
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
